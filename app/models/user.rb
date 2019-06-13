270f14de7de40efd0fb95ec5446bec964e24fa8f
morinaka <morinakakoki@gmail.com>
2019-06-13 22:44:24 +0900

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,16 @@ end
     def forget
       update_attribute(:remember_digest, nil)
     end
+    # アカウントを有効にする
+    def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+    end
+
+    # 有効化用のメールを送信する
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
 
     private
 
