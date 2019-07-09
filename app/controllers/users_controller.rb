ef9eb991ce54ab5f5c4a505f60f20a94d5d1d36d
morinaka <morinakakoki@gmail.com>
2019-07-09 23:26:34 +0900

    Add user following  一応全部行ったけどページネーションはあきらめてすべて表示している。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,14 +48,14 @@ end
 def following
    @title = "Following"
    @user  = User.find(params[:id])
-   @users = @user.following.paginate(page: params[:page])
+   @users = @user.following.all
    render 'show_follow'
  end
 
  def followers
    @title = "Followers"
    @user  = User.find(params[:id])
-   @users = @user.followers.paginate(page: params[:page])
+   @users = @user.followers.all
    render 'show_follow'
  end
 
---
+++ hello.rb
@@ -0,0 +1,11 @@
+class Ohayo
+attr_accessor :name
+def initialize(name)
+  @name=name
+end
+
+  def hello
+puts "hello"+ @name
+end
+
+end
